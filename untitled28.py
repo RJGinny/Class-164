34782c7a18303fc1b50713f90abfa9dabb1e8a0f
RJGinny <92090042+RJGinny@users.noreply.github.com>
2022-05-16 15:34:29 +0530

    Add files via upload

---
+++ untitled28.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 16 15:08:15 2022
+
+@author: riddhiekajain
+"""
+
+from tkinter import *
+
+root = Tk()
+root.title("PLANET ENCYCLOPEDIA")
+root.geometry("500x500")
+root.configure(background="lightblue")
+
+label_planet = Label(root, text="Planet : ", bg="lightblue")
+label_planet_name = Label(root,font=("courier", 18, "bold"),bg="lightblue")
+label_planet_image = Label(root,bg="gold2", highlightthickness=5, borderwidth=2, relief=SOLID)
+label_planet_gravity_radius = Label(root,font=("castellar"),bg="lightblue")
+label_planet_info = Label(root, font=("courier", 10, "bold"), bg="lightblue", wraplength=500)
+
+def PlanetInfo():
+    print("hi")
+    
+btn = Button(root, text="Show Planet Info", command=PlanetInfo)
+btn.place(relx=0.5, rely=0.18, anchor=CENTER)
+
+label_planet.place(relx=0.2, rely=0.1, anchor=CENTER)
+label_planet_name.place(relx=0.5, rely=0.25, anchor=CENTER)
+label_planet_image.place(relx=0.5, rely=0.5, anchor=CENTER)
+label_planet_gravity_radius.place(relx=0.5, rely=0.8, anchor=CENTER)
+label_planet_info.place(relx=0.5, rely=0.9, anchor=CENTER)
+
+
+
+root.mainloop()
